6a7365f12c6b62bfc36f85df7cdef257d44f6fb9
“Tem <torederu@gmail.com>
2025-07-27 14:54:12 +0100

    Update lifestyle csv name

--- components/lifestyle_tab.py
+++ components/lifestyle_tab.py
@@ -6,7 +6,7 @@ from supabase_utils import get_user_supabase
 def lifestyle_tab(username):
     st.markdown("<h1>Lifestyle</h1>", unsafe_allow_html=True)
     user_supabase = get_user_supabase()
-    lifestyle_file = f"{username}/lifestyle_data.csv"
+    lifestyle_file = f"{username}/lifestyle.csv"
     try:
         lifestyle_bytes = user_supabase.storage.from_("data").download(lifestyle_file)
         lifestyle_df = pd.read_csv(io.BytesIO(lifestyle_bytes))
